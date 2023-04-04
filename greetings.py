583f97ef69aef91692d6451df179cfb2894fe695
imjayapple <imjayapple@gmail.com>
2023-04-03 18:45:00 -0700

    Initial commit, greetings program for importing into the main program

---
+++ greetings.py
@@ -0,0 +1,13 @@
+def hello(name):
+    print(f"Hello, {name}!")
+
+def goodbye(name):
+    print(f"Goodbye, {name}!")
+
+def main():
+    print("This message is printed when 'greetings.py' is run directly.")
+    hello("John")
+    goodbye("John")
+
+if __name__ == "__main__":
+    main()
